0fe503e16f4a2ca9b1351bc6fcbabf54a03e4a85
Ong Min Siang <ongminsiang@gmail.com>
2024-09-16 02:44:34 +0800

    added reload function to keep the server alive

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import cors from "cors";
 import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config();
 
@@ -18,6 +19,28 @@ app.use(
     changeOrigin: true,
   })
 );
+
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+function reloadServer() {
+  axios
+    .get(process.env.APP_URL ?? "")
+    .then((response) => {
+      console.log(
+        `Reloaded at ${new Date().toISOString()}: Status Code ${
+          response.status
+        }`
+      );
+    })
+    .catch((error) => {
+      console.error(
+        `Error reloading at ${new Date().toISOString()}:`,
+        error.message
+      );
+    });
+}
+
+setInterval(reloadServer, interval);
 
 // Start the server
 const port = 3030;
